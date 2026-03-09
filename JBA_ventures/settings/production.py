9e851ff36dc5cdc9b66089f7252f1d372a23cc61
juwonthefirst <ajibolajuwon5@gmail.com>
2026-03-09 18:10:29 +0100

    feat: added storages into settings and created a production settings file;feat:added boto3 clud storage for production enviroment

--- JBA_ventures/settings/production.py
+++ JBA_ventures/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = False
 
@@ -6,7 +7,41 @@ DEBUG = False
 # outdated JavaScript / CSS assets being served from cache
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+MEDIA_URL = None
+MEDIA_ROOT = None
+STORAGES = {
+    "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    },
+}
+DATABASES["default"]["OPTIONS"] = {
+    "sslmode": "require",
+}
+AWS_ACCESS_KEY_ID = os.getenv("STORAGE_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("STORAGE_SECRET_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = os.getenv("STORAGE_REGION_ENDPOINT")
+AWS_S3_FILE_OVERWRITE = True
+AWS_S3_REGION_NAME = os.getenv("STORAGE_REGION")
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+AWS_S3_USE_SSL = True
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SAMESITE = "None"
+
+DEBUG = False
+
+MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")
+
+ALLOWED_HOSTS = [
+    os.getenv("HOST_NAME"),
+]
 
 try:
     from .local import *
