0883e0bebab4a44bddca9863aa0d3779145a05e1
juwonthefirst <ajibolajuwon5@gmail.com>
2026-03-14 16:12:53 +0100

    feat: integrate dj-database-url for dynamic database configuration and update dependencies

--- JBA_ventures/settings/base.py
+++ JBA_ventures/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
+import os
+import dj_database_url
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = PROJECT_DIR.parent
@@ -88,13 +90,14 @@ WSGI_APPLICATION = "JBA_ventures.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DEFAULT_DB = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DB_URL", DEFAULT_DB),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
