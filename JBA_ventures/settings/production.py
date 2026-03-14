15a8199465f2e06d6b1a816a012553ee9f057821
juwonthefirst <ajibolajuwon5@gmail.com>
2026-03-14 16:59:59 +0100

    fix: set DEBUG to True and update AWS_S3_FILE_OVERWRITE to False in production settings

--- JBA_ventures/settings/production.py
+++ JBA_ventures/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-# DEBUG = False
+DEBUG = True
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
@@ -22,7 +22,7 @@ AWS_ACCESS_KEY_ID = os.getenv("STORAGE_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.getenv("STORAGE_SECRET_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL = os.getenv("STORAGE_REGION_ENDPOINT")
-AWS_S3_FILE_OVERWRITE = True
+AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = os.getenv("STORAGE_REGION")
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
